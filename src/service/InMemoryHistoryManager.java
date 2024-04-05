47551ef022a6c863864aa39e24957f1be000c35f
lazarx77 <lazarx77@yandex.ru>
2024-04-05 16:46:26 +0400

    1) Updated InMemoryHistoryManager

--- src/service/InMemoryHistoryManager.java
+++ src/service/InMemoryHistoryManager.java
@@ -1,6 +1,7 @@
 package service;
 
 import model.Task;
+import model.TaskStatus;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -8,10 +9,17 @@ import java.util.List;
 public class InMemoryHistoryManager implements HistoryManager {
 
     public List<Task> history = new ArrayList<>();
+    private final int HISTORY_LIST_LIMIT = 10;
 
     @Override
     public void add(Task task) {
-        if (history.size() < 10 ) {
+        String taskName = task.getTaskName(task.getId());
+        int taskId = task.getId();
+        String taskDescription = task.getDescription();
+        TaskStatus taskStatus = task.getStatus();
+
+
+        if (history.size() < HISTORY_LIST_LIMIT) {
             history.add(task);
         } else {
             history.removeFirst();
