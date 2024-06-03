d80dd6a7d9ccec7154165b5c497a8ab8868f84ab
lazarx77 <lazarx77@yandex.ru>
2024-06-03 11:26:41 +0400

    fix/ HttpTaskServer updated with main, HttpServer created

--- src/service/HttpTaskServer.java
+++ src/service/HttpTaskServer.java
@@ -1,4 +1,28 @@
 package service;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
 
-public class HttpTaskServer {
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
+public class HttpTaskServer implements HttpHandler {
+
+    private static final int PORT = 8080;
+
+
+    public static void main(String[] args) throws IOException {
+
+        HttpServer httpServer = HttpServer.create(new InetSocketAddress(PORT), 0);
+
+
+    }
+
+
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+
+    }
 }
+
+
